d9e7e3bc37569b99223ec9ff979f9d28c7a4f8c1
Nanimal20 <112648377+Nanimal20@users.noreply.github.com>
2022-09-20 22:03:48 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,104 +1,131 @@
-#include "TowersOfHanoiGame.cpp"
+// TowerHannoiGame.cpp : Defines the entry point for the console application.
+//
+
+#include "StackQueue.h"
 #include <iostream>
 #include <string>
 #include <vector>
-#include <cmath>
 
 using namespace std;
 
-int main(){
-	std::cout << "\033[2J\033[1;1H";
-
-	int discCount = 4;
-	int stackCount = 3;
-
-	TowersOfHanoiGame game = TowersOfHanoiGame(stackCount, discCount);
-
-	bool wonGame = false;
-
-	std::cout << std::endl;
-	while (wonGame == false){
-
-		game.PrintTowers();
-		
-		std::string line;
-
-		std::cout << "Stack to move from and to (Format like: '1 3'): ";
-		getline(cin, line);
-
-		if (line == "-1"){
-			break;
-		}
-
-		std::vector<std::string> output;
-		std::string::size_type prev_pos = 0, pos = 0;
-
-		while((pos = line.find(" ", pos)) != std::string::npos){
-			std::string substring(line.substr(prev_pos, pos - prev_pos));
-			output.push_back(substring);
-			prev_pos = ++pos;
-		}
-		output.push_back(line.substr(prev_pos, pos - prev_pos));
-		
-		std::cout << "\033[2J\033[1;1H";
-
-		if (output.size() != 2){
-			std::cout << "Invalid input received" << std::endl;
-		} else {
-
-			std::cout << "\r";
-
-			int from = atoi(output[0].c_str());
-			int to = atoi(output[1].c_str());
-
-			MoveResult result = game.MoveDisc(from, to);
-			
-			switch(result){
-
-				case MoveResult::VALID_MOVE:
-					std::cout << "Moved from " << from << " to " << to << std::endl;
-					break;
-
-				case MoveResult::EMPTY_STACK:
-					std::cout << "Cannot move a ring from an empty stack." << std::endl;
-					break;
-
-				case MoveResult::INVALID_MOVE:
-					std::cout << "Cannot place a ring on top of a smaller ring." << std::endl;
-					break;
-
-				case MoveResult::INVALID_STACK:
-					std::cout << from << " or " << to << " is not a valid stack." << std::endl;
-					break;
-				case MoveResult::SAME_STACK:
-					std::cout << "Cannot move a ring to a stack it is already in." << std::endl;
-					break;
-
-				default:
-					break;
-				
-			}
-
-			// Check for win
-			wonGame = game.CheckWin();
-		}
+class TowersOfHannoiGame
+{
+public:
+	TowersOfHannoiGame() : m_GameEnded(false)
+	{
 
 	}
 
-	std::cout << "\033[2J\033[1;1H";
-	std::cout << std::endl;
-	game.SaveToFile("moves.txt");
-	game.PrintTowers();
+	bool IsGameEnded() { return m_GameEnded; }
 
-	if(wonGame){
-		game.PrintWinString();
-	}
-	else {
-		game.PrintLoseString();
+	void PrintTowers()
+	{
+
 	}
 
-	//ArrayBasedQueue moves = game.GetMoves();
+private:
+	bool m_GameEnded;
 
-	
-	return 0;
 };
+
+int main()
+{
+
+	cout << "Enter in moves to make for Towers  of  Hannoi" << endl;
+	cout << "Progam will exit once towers have  been successfully moved of" << endl;
+	cout << "or string of -1 is entered. Moves must be entired in format of" << endl;
+	cout << "<disk>,<column from>,<column to>   NOTE no spaces!!!!" << endl;
+
+	TowersOfHannoiGame game = TowersOfHannoiGame();
+
+	bool receivedEndToken = false;
+
+	while (!receivedEndToken && !game.IsGameEnded())
+	{
+		std::string inputLine;
+		game.PrintTowers();
+		cout << "Enter Move " << endl;
+		getline(cin, inputLine);
+		if (inputLine == "-1")
+		{
+			receivedEndToken = true;
+		}
+		else
+		{
+			std::vector<std::string> output;
+			std::string::size_type prev_pos = 0, pos = 0;
+			// Snippet from https://stackoverflow.com/questions/5167625/splitting-a-c-stdstring-using-tokens-e-g
+			// tokenizing a string
+			while ((pos = inputLine.find(",", pos)) != std::string::npos)
+			{
+				std::string substring(inputLine.substr(prev_pos, pos - prev_pos));
+				output.push_back(substring);
+				prev_pos = ++pos;
+			}
+			//Need to get last token
+			output.push_back(inputLine.substr(prev_pos, pos - prev_pos)); // Last word
+
+			if (output.size() != 3)
+			{
+				receivedEndToken = true;
+				cout << "Invalid input recieved = " + inputLine << endl;
+			}
+			else
+			{
+				std::string disk = output[0];
+				std::string from = output[1];
+				std::string to = output[2];
+
+				if (disk.size() == 0 || from.size() == 0 || to.size() == 0)
+				{
+					receivedEndToken = true;
+					cout << "Invalid input recieved = " + inputLine << endl;
+				}
+
+				int diskId = atoi(disk.c_str()); 
+				int fromId = atoi(from.c_str());
+				int toId = atoi(to.c_str());
+				//if any number is zero we didn't have a integer
+				if (diskId == 0 || fromId == 0 || toId == 0)
+				{
+					receivedEndToken = true;
+					cout << "Invalid input recieved = " + inputLine << endl;
+				}
+
+				cout << "Disk " << diskId << " From " << fromId << " To " << toId << endl;
+
+				
+
+			}
+		}
+
+
+
+	}
+
+    return 0;
+}
+
+int stacksize;
+stacksize = game.size(from);
+size = game.size(from);
+remove = game.pop(diskId, fromId, size, disk, toId);
+game.pop(diskId, fromId, size, disk, toId);
+end = game.IsGameEnded();
+
+if (remove == true){
+	size = game.size(to);
+	game.push(diskId, toId, size, disk, fromId, stacksize);
+}
+else{
+	cout << "Choose the number at the top of the stack." << endl ;
+}
+
+if (end == true){
+	cout << " You successfull completed the challenge." << endl;
+	abort;
+}
+
+
+
+
